009c508f5c13f3fb0ef3273cc899e3082647ebe3
VEDHAGIRI1502 <99638288+VEDHAGIRI1502@users.noreply.github.com>
2022-02-28 11:48:38 +0530

    task
    task

---
+++ Taask file.py
@@ -0,0 +1,34 @@
+
+tamil=75
+english=56
+maths=78
+science=89
+social=67
+print(tamil+english+maths+science+social)
+
+#2
+razak=7000
+sasi=90000
+arun=7000
+mani=7000
+leela=8900
+if razak==sasi:
+    print("razak is equal sasi")
+elif sasi==arun:
+    print("sasi is equal arun")
+elif arun==mani:
+    print("arun is equal mani")
+elif mani==leela:
+    print("mani is equal to leela")
+else:
+    print("the condition is fail")
+#3
+name="my name_sasi"
+print(name[8].upper())
+
+#4
+whats_app="today i have lot of messages"
+print(whats_app.title())
+
+sum=("today chief minister is m.k stalin")
+print(sum.upper())
